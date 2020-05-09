c57b0fd57770c683ef62bb6a29a2ff54c2338e28
Cloud-IT-code <63842103+Cloud-IT-code@users.noreply.github.com>
2020-05-09 11:32:35 +0700

    Create defer.js

---
+++ defer.js
@@ -0,0 +1,11 @@
+//<![CDATA[
+// Page Navigation
+var perPage=12,
+    numPages=3,
+    firstText="First",
+    lastText="Last",
+    prevText="« Previous",
+    nextText="Next »",
+    urlactivepage=location.href,
+    home_page="/";
+//]]>
