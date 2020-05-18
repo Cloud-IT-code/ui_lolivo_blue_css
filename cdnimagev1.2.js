2df4396ce012d4d8a39c3576f43423ffc171f7f5
Cloud-IT-code <63842103+Cloud-IT-code@users.noreply.github.com>
2020-05-19 00:47:54 +0700

    Update and rename cdnimage.js to cdnimagev1.2.js

--- cdnimagev1.2.js
+++ cdnimagev1.2.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 var images = document.getElementsByTagName('img');
   for (var i = 0; i < images.length; i++) {
-    images[i].src = images[i].src.replace(/.*?:\/\//g , "https://cdn.staticaly.com/img/") + "?format=webp";
+    images[i].src = images[i].src.replace(/.*?:\/\//g , "https://cdn.staticaly.io/img/") + "?format=webp";
   };
 //]]>
